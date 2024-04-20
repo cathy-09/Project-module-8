863482ba157016703b068025d1b30b3fa4fb481b
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2024-04-20 15:48:52 +0300

    if-else statements
    equivalent code using if-else statements instead of ternary operators

--- Program_Code.ino
+++ Program_Code.ino
@@ -6,7 +6,6 @@ LiquidCrystal lcd(13, 12, A2, A3, A4, A5);
 unsigned long int firstArg = 0, secondArg = 0;
 double result = 0;
 double ans = 0;
-//String resultText = "";
 bool calculatable = false;
 String operation = "";
 
@@ -14,7 +13,7 @@ void setup()
 {
   Serial.begin(9600);
   lcd.begin(16, 2);
-  lcd.print("Scientific-Calculator");
+  lcd.print("Scientific-Calc");
   delay(1000);
   lcd.clear();
 }
@@ -29,9 +28,6 @@ void loop() {
   	calculateResult();
   }
   displayResult();
-  //lcd.print(valueA0);
-  //lcd.setCursor(0, 1);
-  //lcd.print(valueA1);
   delay(500);
   lcd.clear();
 }
@@ -40,56 +36,96 @@ void detectButtons() {
   int valueA0 = analogRead(A0);
   int valueA1 = analogRead(A1);
   switch (valueA1) { //switch on the larger keypad (basic operations)
-    case 0: //number 0
-    {
-    	result = (result == 0) ? 0 : result * 10;
-    }
-    	break;
-    case 50: //number 1
-    {
-    	result = (result == 0) ? 1 : result * 10 + 1;
-    }
-    	break;
-    case 100: //number 2
-    {
-    	result = (result == 0) ? 2 : result * 10 + 2;
-    }
-    	break;
-    case 150: //number 3
-    {
-    	result = (result == 0) ? 3 : result * 10 + 3;
-    }
-    	break;
-    case 200: //number 4
-    {
-    	result = (result == 0) ? 4 : result * 10 + 4;
-    }
-    	break;
-    case 250: //number 5
-    {
-    	result = (result == 0) ? 5 : result * 10 + 5;
-    }
-    	break;
-    case 300: //number 6
-    {
-    	result = (result == 0) ? 6 : result * 10 + 6;
-    }
-    	break;
-    case 350: //number 7
-    {
-    	result = (result == 0) ? 7 : result * 10 + 7;
-    }
-    	break;
-    case 400: //number 8
-    {
-    	result = (result == 0) ? 8 : result * 10 + 8;
-    }
-    	break;
-    case 450: //number 9
-    {
-    	result = (result == 0) ? 9 : result * 10 + 9;
-    }
-    	break;
+    case 0: // number 0
+	{
+    	if (result == 0) {
+        result = 0;
+    	} else {
+        	result *= 10;
+    	}
+	}
+	break;
+	case 50: // number 1
+	{
+    	if (result == 0) {
+        result = 1;
+    	} else {
+        result = result * 10 + 1;
+    	}
+	}
+	break;
+	case 100: // number 2
+	{
+    	if (result == 0) {
+        result = 2;
+    	} else {
+        	result = result * 10 + 2;
+    	}
+	}
+	break;
+	case 150: // number 3
+	{
+    	if (result == 0) {
+        result = 3;
+    	} else {
+    	    result = result * 10 + 3;
+    	}
+	}
+    break;
+	case 200: // number 4
+	{
+    	if (result == 0) {
+        result = 4;
+    	} else {
+        	result = result * 10 + 4;
+    	}
+	}
+	break;
+	case 250: // number 5
+	{
+    	if (result == 0) {
+        result = 5;
+    	} else {
+        	result = result * 10 + 5;
+    	}
+	}
+	break;
+	case 300: // number 6
+	{
+    	if (result == 0) {
+        result = 6;
+    	} else {
+        	result = result * 10 + 6;
+    	}
+	}
+	break;
+	case 350: // number 7
+	{
+    	if (result == 0) {
+        result = 7;
+    	} else {
+        	result = result * 10 + 7;
+    	}
+	}
+	break;
+	case 400: // number 8
+	{
+    	if (result == 0) {
+        result = 8;
+    	} else {
+        	result = result * 10 + 8;
+    	}
+	}
+	break;
+	case 450: // number 9
+	{
+    	if (result == 0) {
+        result = 9;
+    	} else {
+        	result = result * 10 + 9;
+    	}
+	}
+	break;
     case 500: //=
     {
     	secondArg = result;
